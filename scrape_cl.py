e3b40f34097d04e344e2430bbf4d5324251eed17
Jose Reyes <josreyes53@gmail.com>
2018-04-06 21:38:14 -0400

    Takes url as parameter

--- scrape_cl.py
+++ scrape_cl.py
@@ -1,12 +1,20 @@
 from lxml import html
 import requests
+from configargparse import ArgParser
+from shlex import quote
 
-page = requests.get('http://www.comiclist.com/index.php/lists/comiclist-new-comic-book-releases-list-for-12-20-2017')
+p = ArgParser()
+
+p.add('url', help='ComicList weekly release URL')
+
+args = p.parse_args()
+
+page = requests.get(args.url)
 tree = html.fromstring(page.content)
 
 pubs = tree.xpath('//b/u/text()')
 titles = tree.xpath('//p/a/text()')
 
 for i in pubs:
-	if "PUBLISHER" not in i and "Games - " not in i and "Merchandise" not in i:
+	if "PUBLISHER" not in i and "Games - " not in i and "Merchandise" not in i and "Cards - " not in i:
 		print(i)
